7e6a674517106cd2561c8a928783ab98cf35c5db
Martin Pauly <martinpauly@googlemail.com>
2020-12-12 16:13:44 +0100

    Add connected graph

--- plot_testgraphs.py
+++ plot_testgraphs.py
@@ -2,14 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1D chain plot
-sigma, dim = np.loadtxt('data/dimension_1d.dat', unpack=True)
-
+sigma, dim = np.loadtxt('data/dimension_1d_short.dat', unpack=True)
 mask = ~np.isnan(dim)
-
 plt.plot(sigma[mask], dim[mask])
 plt.axhline(1, ls='--')
 plt.axvline(26, ls='--')
 plt.axvline(52, ls='--')
+
+plt.xlabel('$\\sigma$')
+plt.ylabel('$d_{\\rm spec}$')
+plt.savefig('plots/dimension_1d_short.png')
+
+# 1D chain plot
+plt.clf()
+sigma, dim = np.loadtxt('data/dimension_1d.dat', unpack=True)
+mask = ~np.isnan(dim)
+plt.plot(sigma[mask], dim[mask])
+sigma, dim = np.loadtxt('data/dimension_1d_connected.dat', unpack=True)
+mask = ~np.isnan(dim)
+plt.plot(sigma[mask], dim[mask])
+plt.axhline(1, ls='--')
+plt.axvline(100, ls='--')
 
 plt.xlabel('$\\sigma$')
 plt.ylabel('$d_{\\rm spec}$')
--- testgraphs.cpp
+++ testgraphs.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "Snap.h"
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -15,8 +15,8 @@
 // The implementation roughly corresponds to a breadth-first search tree.
 // Inspired by DoBfs
 template <class PGraph>
-std::vector<double> traverseBfsTree(const PGraph &Graph, const int &start_node, const int &max_depth,
-                                    std::ofstream &dimfile) {
+std::vector<double> spectralDimensionAtNode(const PGraph &Graph, const int &start_node, const int &max_depth,
+                                            std::ofstream &dimfile) {
   // setup data structures
   TSnapQueue<int> queue;
   std::vector<THash<TInt, double>> levelProbabilities(max_depth + 1);
@@ -103,70 +103,97 @@ std::vector<double> traverseBfsTree(const PGraph &Graph, const int &start_node, 
 int main(int argc, char *argv[]) {
   typedef PUNGraph PGraph; // undirected graph
 
-  // Create the graph
+  // =============== 1D ring ===============================
   PGraph G = PGraph::TObj::New();
   int ring_length = 26;
   for (int n = 0; n < ring_length; n++) {
     G->AddNode(); // if no parameter is given, node ids are 0,1,...
   }
 
-  // Build a ring
   for (int n = 0; n < ring_length; n++) {
     G->AddEdge(n, (n + 1) % ring_length);
   }
 
-  // Adding random edges
-  // for (int e = 0; e < 10; e++) {
-  //  const int NId1 = G->GetRndNId();
-  //  const int NId2 = G->GetRndNId();
-  //  if (G->AddEdge(NId1, NId2) != -2) {
-  //    printf("  Edge %d -- %d added\n", NId1, NId2);
-  //  } else {
-  //    printf("  Edge %d -- %d already exists\n", NId1, NId2);
-  //  }
-  //}
-
-  // TSnap::DrawGViz(G, gvlDot, "graph.png");
-
   std::ofstream dimfile;
-  dimfile.open("data/dimension_1d.dat");
+  dimfile.open("data/dimension_1d_short.dat");
   dimfile << "# dimensions for a ring of length " << ring_length << "\n";
-  // arguments here are start_node and depth
-  auto treeCounts = traverseBfsTree(G, 7, 150, dimfile);
+  auto treeCounts = spectralDimensionAtNode(G, 7, 150, dimfile);
   dimfile.close();
 
+  // =============== 1D ring ===============================
+  G = PGraph::TObj::New();
+  ring_length = 100;
+  for (int n = 0; n < ring_length; n++) {
+    G->AddNode(); // if no parameter is given, node ids are 0,1,...
+  }
+
+  for (int n = 0; n < ring_length; n++) {
+    G->AddEdge(n, (n + 1) % ring_length);
+  }
+
+  dimfile.open("data/dimension_1d.dat");
+  dimfile << "# dimensions for a ring of length " << ring_length << "\n";
+  treeCounts = spectralDimensionAtNode(G, 7, 150, dimfile);
+  dimfile.close();
+
+  // =============== 1D ring with random connections ===============================
+  PGraph G1a = PGraph::TObj::New();
+  ring_length = 100;
+  for (int n = 0; n < ring_length; n++) {
+    G1a->AddNode(); // if no parameter is given, node ids are 0,1,...
+  }
+
+  for (int n = 0; n < ring_length; n++) {
+    G1a->AddEdge(n, (n + 1) % ring_length);
+  }
+
+  const int nr_of_random_connections = 50;
+
+  // Adding random edges
+  int edges_added = 0;
+  while (edges_added < nr_of_random_connections) {
+    const int NId1 = G->GetRndNId();
+    const int NId2 = G->GetRndNId();
+    // this if condition checks if an edge already exists
+    if (G->AddEdge(NId1, NId2) != -2) {
+      edges_added++;
+    }
+  }
+
+  TSnap::DrawGViz(G, gvlDot, "graph.png");
+
+  std::ofstream dimfile1a;
+  dimfile1a.open("data/dimension_1d_connected.dat");
+  dimfile1a << "# dimensions for a ring with " << nr_of_random_connections
+            << " additional random connections of length " << ring_length << "\n";
+  auto treeCounts1a = spectralDimensionAtNode(G, 7, 150, dimfile1a);
+  dimfile1a.close();
+
   // ================== 2D lattice ===================================
-  // Create the graph
   PGraph G2 = PGraph::TObj::New();
   for (int n = 0; n < EDGE_LENGTH * EDGE_LENGTH; n++) {
     G2->AddNode(); // if no parameter is given, node ids are 0,1,...
   }
-
-  // Build a ring
+  // Build a square
   for (int n = 0; n < EDGE_LENGTH - 1; n++) {
     for (int m = 0; m < EDGE_LENGTH - 1; m++) {
       G2->AddEdge(INDEXAT2D(n, m), INDEXAT2D(n + 1, m));
       G2->AddEdge(INDEXAT2D(n, m), INDEXAT2D(n, m + 1));
     }
   }
-
-  const typename PGraph::TObj::TNodeI NodeI = G2->GetNI(INDEXAT2D(50, 50));
-  const int noteDegree = NodeI.GetOutDeg();
-
   std::ofstream dimfile2;
   dimfile2.open("data/dimension_2d.dat");
   dimfile2 << "# dimensions for a square \n";
-  auto treeCounts2 = traverseBfsTree(G2, INDEXAT2D(50, 50), 50, dimfile2);
+  auto treeCounts2 = spectralDimensionAtNode(G2, INDEXAT2D(50, 50), 100, dimfile2);
   dimfile2.close();
 
   // ================== 3D lattice ===================================
-  // Create the graph
   PGraph G3 = PGraph::TObj::New();
   for (int n = 0; n < EDGE_LENGTH * EDGE_LENGTH * EDGE_LENGTH; n++) {
     G3->AddNode(); // if no parameter is given, node ids are 0,1,...
   }
 
-  // Build a ring
+  // Build a cube
   for (int n1 = 0; n1 < EDGE_LENGTH - 1; n1++) {
     for (int n2 = 0; n2 < EDGE_LENGTH - 1; n2++) {
       for (int n3 = 0; n3 < EDGE_LENGTH - 1; n3++) {
@@ -177,21 +204,10 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // Adding random edges
-  // for (int e = 0; e < 10; e++) {
-  //  const int NId1 = G->GetRndNId();
-  //  const int NId2 = G->GetRndNId();
-  //  if (G->AddEdge(NId1, NId2) != -2) {
-  //    printf("  Edge %d -- %d added\n", NId1, NId2);
-  //  } else {
-  //    printf("  Edge %d -- %d already exists\n", NId1, NId2);
-  //  }
-  //}
-
   std::ofstream dimfile3;
   dimfile3.open("data/dimension_3d.dat");
   dimfile3 << "# dimensions for a cube \n";
-  auto treeCounts3 = traverseBfsTree(G3, INDEXAT3D(50, 50, 50), 50, dimfile3);
+  auto treeCounts3 = spectralDimensionAtNode(G3, INDEXAT3D(50, 50, 50), 100, dimfile3);
   dimfile3.close();
 
   // the following are approximate neighbourhood functions - might be
