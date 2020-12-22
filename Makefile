49d7f531024d941f59b6b83cc55f8db8c91b080a
Martin Pauly <martinpauly@googlemail.com>
2020-12-22 16:40:11 +0100

    Europe OSM dataset

--- Makefile
+++ Makefile
@@ -17,9 +17,10 @@ snap:
 	cd snap/snap-core && $(MAKE) lib
 
 graph_data:
-	 wget -O - http://snap.stanford.edu/data/roadNet-PA.txt.gz | gunzip -c > graphs/roadNet-PA.txt
-	 wget -O - http://snap.stanford.edu/data/as-skitter.txt.gz | gunzip -c > graphs/as-skitter.txt
-	 wget -O tmp_network.zip http://nrvis.com/download/data/bn/bn-fly-drosophila_medulla_1.zip && unzip -j -d graphs/tmp tmp_network.zip && mv graphs/tmp/bn-fly-drosophila_medulla_1.edges graphs/fly-drosophila_edges.txt && rm -r graphs/tmp && rm tmp_network.zip
+	 wget -O - http://snap.stanford.edu/data/roadNet-PA.txt.gz | gunzip -c > graphs/data/roadNet-PA.txt
+	 wget -O - http://snap.stanford.edu/data/as-skitter.txt.gz | gunzip -c > graphs/data/as-skitter.txt
+	 wget -O tmp_network.zip http://nrvis.com/download/data/bn/bn-fly-drosophila_medulla_1.zip && unzip -j -d graphs/tmp tmp_network.zip && mv graphs/tmp/bn-fly-drosophila_medulla_1.edges graphs/data/fly-drosophila_edges.txt && rm -r graphs/tmp && rm tmp_network.zip
+	 wget -O - https://suitesparse-collection-website.herokuapp.com/MM/DIMACS10/europe_osm.tar.gz | tar -xz -C graphs/data/
 
 diffusion_test:
 	for i in {1..9}; do ./random_walk.x -l 200 -d 0.$$i -s 5050 -o data/2dtest/d0$$i.dat graphs/2d_lattice_100.dat; done
@@ -28,5 +29,4 @@ diffusion_test:
 clean:
 	rm -f *.x
 	rm -f graphs/*.dat
-	rm -f data/*.dat
 	rm -f plots/*.png
--- make_datagraphs.cpp
+++ make_datagraphs.cpp
@@ -1,6 +1,7 @@
 #include "Snap.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
@@ -19,23 +20,55 @@ int main(int argc, char *argv[]) {
   // =============== PA streets ===============================
   {
     std::cout << "== Writing Pensylvania street network graph ==" << std::endl;
-    PNGraph G = TSnap::LoadEdgeList<PNGraph>("graphs/roadNet-PA.txt", 0, 1);
+    PNGraph G = TSnap::LoadEdgeList<PNGraph>("graphs/data/roadNet-PA.txt", 0, 1);
     // Here we are converting to an undirected graph for now
     save_graph_to_file(TSnap::ConvertGraph<PUNGraph>(G), graph_dir + "roadnet_pa.dat");
   }
   // =============== Internet topology ===============================
   {
     std::cout << "== Writing AS Skitter Internet topology graph ==" << std::endl;
-    PUNGraph G = TSnap::LoadEdgeList<PUNGraph>("graphs/as-skitter.txt", 0, 1);
+    PUNGraph G = TSnap::LoadEdgeList<PUNGraph>("graphs/data/as-skitter.txt", 0, 1);
     // Here we are converting to an undirected graph for now
     save_graph_to_file(G, graph_dir + "as-skitter.dat");
   }
   // =============== Drosophila brain ===============================
   {
     std::cout << "== Writing Drosophila Brain graph ==" << std::endl;
-    PUNGraph G = TSnap::LoadEdgeList<PUNGraph>("graphs/fly-drosophila_edges.txt", 0, 1);
+    PUNGraph G = TSnap::LoadEdgeList<PUNGraph>("graphs/data/fly-drosophila_edges.txt", 0, 1);
     // Here we are converting to an undirected graph for now
     save_graph_to_file(G, graph_dir + "fly-drosophila.dat");
+  }
+  // =============== Europe OSM map ===============================
+  {
+    std::cout << "== Writing Europe OSM graph ==" << std::endl;
+    fstream osmfile;
+    int rows, columns, entries;
+    osmfile.open("graphs/data/europe_osm/europe_osm.mtx", ios::in);
+    if (osmfile.is_open()) {
+      std::string line;
+      int nr_of_nodes = 0, nr_of_edges = 0;
+      while (std::getline(osmfile, line)) {
+        if (line[0] == '%')
+          continue;
+        std::istringstream iss(line);
+        iss >> rows, columns, entries;
+        break;
+      }
+
+      PGraph G = PUNGraph::TObj::New();
+      for (int n = 0; n < rows; n++) {
+        G->AddNode();
+        nr_of_nodes++;
+      }
+      int in_id, out_id;
+      while (osmfile >> in_id >> out_id) {
+        G->AddEdge(in_id - 1, out_id - 1);
+        nr_of_edges++;
+      }
+      std::cout << "  # Nodes: " << nr_of_nodes << "  # Edges: " << nr_of_edges << std::endl;
+      save_graph_to_file(G, graph_dir + "europe_osm.dat");
+      osmfile.close();
+    }
   }
 
   printf("\n Done writing graphs to ");
