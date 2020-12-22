cab2c74e2d1dc15e46a37591f12379df58e9ac2e
Martin Pauly <martinpauly@googlemail.com>
2020-12-22 11:17:21 +0100

    Reorganize plot scripts

---
+++ plots/plot_2dtest.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+for j in range(9):
+    i = 9 - j
+    dimfile = 'data/2dtest/d0{}.dat'.format(i)
+    print("Reading file {} ".format(dimfile))
+    # 1D chain plot
+    startnode, sigma, dim = np.loadtxt(dimfile, unpack=True)
+    startnodes = np.unique(startnode)
+
+    for node in startnodes:
+        mask = (node == startnode) & (~np.isnan(dim))
+    plt.plot(sigma[mask], dim[mask], label='$d=0.{}$'.format(i))
+
+plt.legend()
+plt.xlabel('$\\sigma$')
+plt.ylabel('$d_{\\rm spec}$')
+plt.savefig('plots/2dtest.pdf')
