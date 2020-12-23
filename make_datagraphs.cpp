3d7b116c1a96502fd15ec31e82809a5cb6a293a3
Martin Pauly <martinpauly@googlemail.com>
2020-12-23 11:15:38 +0100

    New reduction algo

--- make_datagraphs.cpp
+++ make_datagraphs.cpp
@@ -44,9 +44,11 @@ int main(int argc, char *argv[]) {
     fstream osmfile;
     int rows, columns, entries;
     osmfile.open("graphs/data/europe_osm/europe_osm.mtx", ios::in);
+    int nr_of_nodes = 0, nr_of_edges = 0;
+    PGraph G = PUNGraph::TObj::New();
     if (osmfile.is_open()) {
       std::string line;
-      int nr_of_nodes = 0, nr_of_edges = 0;
+
       while (std::getline(osmfile, line)) {
         if (line[0] == '%')
           continue;
@@ -55,7 +57,6 @@ int main(int argc, char *argv[]) {
         break;
       }
 
-      PGraph G = PUNGraph::TObj::New();
       for (int n = 0; n < rows; n++) {
         G->AddNode();
         nr_of_nodes++;
@@ -68,27 +69,73 @@ int main(int argc, char *argv[]) {
       std::cout << "  # Nodes: " << nr_of_nodes << "  # Edges: " << nr_of_edges << std::endl;
       save_graph_to_file(G, graph_dir + "europe_osm.dat");
       osmfile.close();
-
-      std::cout << "== Reducing Europe OSM graph ==" << std::endl;
-
-      int nodes_reduced = 0;
-      int node0, node1;
-
-      for (int i = 0; i < nr_of_nodes; i++) {
-        const typename PGraph::TObj::TNodeI NodeI = G->GetNI(i);
-        const int nodeDegree = NodeI.GetOutDeg();
-        if (nodeDegree == 2) {
-          node0 = NodeI.GetOutNId(0);
-          node1 = NodeI.GetOutNId(1);
-          G->DelNode(i);
-          nodes_reduced++;
-          if (!G->IsEdge(node0, node1))
-            G->AddEdge(node0, node1);
-        }
-      }
-      std::cout << "  Reduced by " << nodes_reduced << " nodes - saving";
-      save_graph_to_file(G, graph_dir + "europe_osm_reduced.dat");
     }
+
+    std::cout << "== Reducing Europe OSM graph ==" << std::endl;
+
+    int nodes_reduced = 0;
+    int node0, node1;
+    bool nodes_removed;
+
+    // We are reducing the graph because it still contains many long roads composed of many nodes
+    // 1) Looping over all edges
+    // 2) If both nodes leading into an edge have degree two we can get rid of one of the nodes and the edge
+    // 3) Rewiring then requires to add one edge between node j and the other child node of i
+    // This looks as if it could be formulated as a loop over child nodes, but I don't know exactly how GetOutNId
+    // behaves if the graph is changing during the loop
+    for (int i = 0; i < nr_of_nodes; i++) {
+      if (!G->IsNode(i))
+        continue;
+      const typename PGraph::TObj::TNodeI NodeI = G->GetNI(i);
+      const int nodeIDegree = NodeI.GetOutDeg();
+      if (nodeIDegree != 2)
+        continue;
+
+      do {
+        const int j1 = NodeI.GetOutNId(0);
+        const typename PGraph::TObj::TNodeI NodeJ1 = G->GetNI(j1);
+        const int nodeJ1Degree = NodeJ1.GetOutDeg();
+        const int j2 = NodeI.GetOutNId(1);
+        const typename PGraph::TObj::TNodeI NodeJ2 = G->GetNI(j2);
+        const int nodeJ2Degree = NodeJ2.GetOutDeg();
+
+        int removalNode;
+        if (nodeJ1Degree == 2) {
+          nodes_removed = true;
+          removalNode = j1;
+          const int nodeJ1Degree = NodeJ1.GetOutDeg();
+        } else if (nodeJ2Degree == 2) {
+          nodes_removed = true;
+          removalNode = j2;
+        } else {
+          nodes_removed = false;
+        }
+
+        // if one of the child nodes are degree two we can get rid of that node
+        if (nodes_removed) {
+          const typename PGraph::TObj::TNodeI RemovedNode = G->GetNI(removalNode);
+          const int childId0 = RemovedNode.GetOutNId(0);
+          const int childId1 = RemovedNode.GetOutNId(1);
+          // ... except if we already form a triangle
+          if (childId0 == j1 || childId1 == j1 || childId0 == j2 || childId1 == j2) {
+            nodes_removed = false;
+            continue;
+          }
+          G->DelNode(removalNode);
+          nodes_reduced++;
+          nodes_removed = true;
+
+          if (childId0 != i && !G->IsEdge(childId0, i)) {
+            G->AddEdge(childId0, i);
+          }
+          if (childId1 != i && !G->IsEdge(childId1, i)) {
+            G->AddEdge(childId1, i);
+          }
+        }
+      } while (nodes_removed);
+    }
+    std::cout << "  Reduced by " << nodes_reduced << " nodes - saving";
+    save_graph_to_file(G, graph_dir + "europe_osm_reduced.dat");
   }
 
   printf("\n Done writing graphs to ");
