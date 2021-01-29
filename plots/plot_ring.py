d2b4b5ca7a6934cd3af8a4db8fc1c083a0bfb4ae
Martin Pauly <martinpauly@googlemail.com>
2021-01-29 13:16:48 +0100

    Update ring plots

--- plots/plot_ring.py
+++ plots/plot_ring.py
@@ -1,7 +1,12 @@
 from itertools import cycle
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
+
+matplotlib.rcParams["mathtext.fontset"] = "stix"
+matplotlib.rcParams["font.family"] = "STIXGeneral"
 
 dimfile = "data/dim_ring_random.dat"
 
@@ -29,7 +34,10 @@ plt.title("$\\delta = {}$, varying number of connections".format(reference_delta
 plt.xlabel("$\\sigma$")
 plt.ylabel("$d_{\\rm spec}$")
 plt.xlim(0, 200)
-plt.savefig("plots/out/dim_ring_1.pdf")
+
+fig = plt.gcf()
+fig.set_size_inches(5.52, 3.41)
+fig.savefig("plots/out/dim_ring_1.pdf", bbox_inches="tight")
 
 plt.clf()
 
@@ -43,7 +51,10 @@ plt.title("${}$ connections".format(reference_conn))
 plt.xlabel("$\\sigma$")
 plt.ylabel("$d_{\\rm spec}$")
 plt.xlim(0, 200)
-plt.savefig("plots/out/dim_ring_2.pdf")
+
+fig = plt.gcf()
+fig.set_size_inches(5.52, 3.41)
+fig.savefig("plots/out/dim_ring_2.pdf", bbox_inches="tight")
 
 plt.clf()
 
@@ -59,7 +70,10 @@ chain_length = 100
 for dc in diffusion_const_values:
     mask = dc == diffusion_const
     these_sigmas = sigma[mask]
-    line = plt.plot(these_sigmas, dim[mask], label="$\\delta={:.2f}$".format(dc))
+    these_dim = dim[mask]
+    line = plt.plot(
+        these_sigmas[:-2], these_dim[:-2], label="$\\delta={:.2f}$".format(dc)
+    )
 
     eigenvalue1 = dc * (1.0 - np.cos(2.0 * np.pi * 1.0 / chain_length))
     theoretical_expectation = (
@@ -79,23 +93,48 @@ plt.xlabel("$\\sigma$")
 plt.ylabel("$d_{\\rm spec}$")
 plt.ylim(1e-6, 3)
 plt.semilogy()
-plt.savefig("plots/out/dim_ring_3.pdf")
+
+fig = plt.gcf()
+fig.set_size_inches(5.52, 3.41)
+fig.savefig("plots/out/dim_ring_3.pdf", bbox_inches="tight")
 
 plt.clf()
 
 # fourth plot
+max_sigma_inset = 200
 
 dimfile = "data/dim_ring_random_maxlength.dat"
 print("Reading file {}".format(dimfile))
 rand_conn, sigma, dim = np.loadtxt(dimfile, unpack=True)
 rand_conn_vals = np.unique(rand_conn)
 
+fig, ax1 = plt.subplots()
+
 for rc in rand_conn_vals:
     mask = rc == rand_conn
     these_sigmas = sigma[mask]
-    plt.plot(these_sigmas, dim[mask], label="conns=${:0.0f}$".format(rc))
+    ax1.plot(these_sigmas, dim[mask], label="conns=${:0.0f}$".format(rc))
 
-plt.legend()
-plt.xlabel("$\\sigma$")
-plt.ylabel("$d_{\\rm spec}$")
-plt.savefig("plots/out/dim_ring_4.pdf")
+ax1.legend(loc="lower right")
+ax1.set_xlabel("$\\sigma$")
+ax1.set_ylabel("$d_{\\rm spec}$")
+
+plt.axhline(2, c="tab:green", ls="--")
+ax1.set_xlim([0, np.max(sigma)])
+ax1.set_ylim([0.0, 1.2])
+
+ax2 = plt.axes([0, 0, 1, 1])
+# Manually set the position and relative size of the inset axes within ax1
+ip = InsetPosition(ax1, [0.08, 0.1, 0.45, 0.45])
+ax2.set_axes_locator(ip)
+# ax2.set_xlabel("$\\sigma$")
+# ax2.set_ylabel("$d_{\\rm spec}$")
+
+for rc in rand_conn_vals:
+    mask = (rc == rand_conn) & (sigma < max_sigma_inset)
+    these_sigmas = sigma[mask]
+    ax2.plot(these_sigmas, dim[mask])
+
+fig = plt.gcf()
+fig.set_size_inches(5.52, 3.41)
+fig.savefig("plots/out/dim_ring_4.pdf", bbox_inches="tight")
