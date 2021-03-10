333ea7a805ba38a218c6ed75a9860289f2f96a9e
Martin Pauly <martinpauly@googlemail.com>
2021-03-10 12:34:51 +0100

    Larger barabasi albert

--- barabasi_albert.cpp
+++ barabasi_albert.cpp
@@ -6,17 +6,9 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[]) {
-
-  const int nr_of_nodes = 1000;
-  const std::vector<int> degrees = {2, 3, 4, 5, 6};
-  const int nr_of_graphs = 5;
-  const int nr_of_startnodes = 25;
-  const int walk_length = 200;
-  const std::vector<double> diffusion_constants = {0.25, 0.5, 0.75};
-
-  std::ofstream dimfile;
-  dimfile.open(walker::DIMENSION_DIR + "barabasi_albert.dat", std::ofstream::out);
+void process_barabasi_albert(std::ofstream &dimfile, const std::vector<int> &degrees,
+                             const std::vector<double> &diffusion_constants, const int &walk_length,
+                             const int &nr_of_graphs, const int &nr_of_startnodes, const int &nr_of_nodes) {
   dimfile << "diffusion_const\tdegree\tsigma\tdim" << std::endl;
 
   const std::function<void(walker::RandomWalk)> progress_monitor = [](const walker::RandomWalk &walk) {
@@ -28,7 +20,8 @@ int main(int argc, char *argv[]) {
     for (double diffusion_constant : diffusion_constants) {
       std::cout << "== Degree " << degree << " diffusion_const " << diffusion_constant << " == " << std::endl;
       std::vector<double> dimension(walk_length, 0);
-      // we are averaging over nr_of_graphs different graphs
+// we are averaging over nr_of_graphs different graphs
+#pragma omp parallel for
       for (int graph = 0; graph < nr_of_graphs; graph++) {
         PUNGraph G;
         G = TSnap::GenPrefAttach(nr_of_nodes, degree);
@@ -49,9 +42,48 @@ int main(int argc, char *argv[]) {
           }
         }
       }
-      for (int i = 1; i < walk_length; i++) {
-        dimfile << diffusion_constant << "\t" << degree << "\t" << i << "\t" << dimension[i] << "\n";
+#pragma omp critical
+      {
+        for (int i = 1; i < walk_length; i++) {
+          dimfile << diffusion_constant << "\t" << degree << "\t" << i << "\t" << dimension[i] << "\n";
+        }
       }
     }
+  }
+}
+
+int main(int argc, char *argv[]) {
+
+  {
+    const int nr_of_nodes = 1000;
+    const std::vector<int> degrees = {2, 3, 4, 5, 6};
+    const int nr_of_graphs = 5;
+    const int nr_of_startnodes = 25;
+    const int walk_length = 200;
+    const std::vector<double> diffusion_constants = {0.25, 0.5, 0.75};
+
+    std::ofstream dimfile;
+    dimfile.open(walker::DIMENSION_DIR + "barabasi_albert.dat", std::ofstream::out);
+
+    process_barabasi_albert(dimfile, degrees, diffusion_constants, walk_length, nr_of_graphs, nr_of_startnodes,
+                            nr_of_nodes);
+
+    dimfile.close();
+  }
+  {
+    const int nr_of_nodes = 100000;
+    const std::vector<int> degrees = {2, 3, 4, 5, 6};
+    const int nr_of_graphs = 5;
+    const int nr_of_startnodes = 25;
+    const int walk_length = 200;
+    const std::vector<double> diffusion_constants = {0.25, 0.5, 0.75};
+
+    std::ofstream dimfile;
+    dimfile.open(walker::DIMENSION_DIR + "barabasi_albert_large.dat", std::ofstream::out);
+
+    process_barabasi_albert(dimfile, degrees, diffusion_constants, walk_length, nr_of_graphs, nr_of_startnodes,
+                            nr_of_nodes);
+
+    dimfile.close();
   }
 }
