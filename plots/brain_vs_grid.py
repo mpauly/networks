adb97d3f60fa1a53b886436380ae06182ea97b58
Martin Pauly <martinpauly@googlemail.com>
2021-03-12 13:57:15 +0100

    Add a comparison between brain and grid

---
+++ plots/brain_vs_grid.py
@@ -0,0 +1,70 @@
+# %%
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib.collections import LineCollection
+from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
+
+# %% [markdown]
+# Below we compare the spectral dimension of a 3D grid
+# to the brain split into roughly 300 regions.
+
+# %%
+dimfile = "../data/dimension/human_brain_regions_300.dat"
+data = pd.read_table(dimfile, comment="#", names=["start_node", "sigma", "dim"])
+
+data_plot = np.array(list(data.groupby("start_node").apply(pd.DataFrame.to_numpy)))
+data_plot = data_plot[:, :, 1:]
+# %%
+fig, ax1 = plt.subplots()
+ax1.set_xlim(data["sigma"].min(), data["sigma"].max())
+ax1.set_ylim(0, 10)
+line_segments = LineCollection(data_plot, alpha=0.2)
+ax1.add_collection(line_segments)
+ax1.axhline(3, c="tab:green", ls="--")
+ax1.set_xlabel("$\\sigma$")
+ax1.set_ylabel("$d_{\\rm spec}$")
+# %% [markdown]
+# The brain is extremly well connected, the spectral dimension
+# decays more or less instantly.
+# %%
+dimfile = "../data/dimension/3d_lattice_7.dat"
+data = pd.read_table(dimfile, comment="#", names=["start_node", "sigma", "dim"])
+
+data_plot = np.array(list(data.groupby("start_node").apply(pd.DataFrame.to_numpy)))
+data_plot = data_plot[:, :, 1:]
+# %%
+plt.clf()
+fig, ax1 = plt.subplots()
+ax1.set_xlim(data["sigma"].min(), data["sigma"].max())
+ax1.set_ylim(0, 10)
+line_segments = LineCollection(data_plot, alpha=0.2)
+ax1.add_collection(line_segments)
+
+ax1.axhline(3, c="tab:green", ls="--")
+ax1.set_xlabel("$\\sigma$")
+ax1.set_ylabel("$d_{\\rm spec}$")
+# %% [markdown]
+# Various of the walkers actually experience a dimension smaller than three.
+# Presumably due to the finite extend.
+# %%
+dimfile = "../data/dimension/3d_lattice_100.dat"
+data = pd.read_table(dimfile, comment="#", names=["start_node", "sigma", "dim"])
+
+data_plot = np.array(list(data.groupby("start_node").apply(pd.DataFrame.to_numpy)))
+data_plot = data_plot[:, :, 1:]
+# %%
+plt.clf()
+fig, ax1 = plt.subplots()
+ax1.set_xlim(data["sigma"].min(), data["sigma"].max())
+ax1.set_ylim(0, 10)
+line_segments = LineCollection(data_plot, alpha=0.2)
+ax1.add_collection(line_segments)
+
+ax1.axhline(3, c="tab:green", ls="--")
+ax1.set_xlabel("$\\sigma$")
+ax1.set_ylabel("$d_{\\rm spec}$")
+# %%
+data[data["dim"].max() == data["dim"]]
+# %%
