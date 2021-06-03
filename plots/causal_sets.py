389828392c615ada1b458ae54d4222b66c02564a
Martin Pauly <martinpauly@googlemail.com>
2021-06-03 22:18:21 +0200

    transitive percolation plots

--- plots/causal_sets.py
+++ plots/causal_sets.py
@@ -71,4 +71,43 @@ plt.ylabel("$d_{\\rm spec}$")
 fig.set_size_inches(5.52, 3.41)
 fig.savefig("../plots/out/causal_set_l30.pdf", bbox_inches="tight")
 
+# %% [markdown]
+# # Transitive Percolations
+# %%
+dimfile = "../data/dimension/transitive_percolations_0.000001.dat"
+data = pd.read_table(dimfile, comment="#", names=["start_node", "sigma", "dim"])
+startnodes = pd.unique(data["start_node"])
+maxsigma = data["sigma"].max()
+# %%
+data_plot = np.array(list(data.groupby("start_node").apply(pd.DataFrame.to_numpy)))
+data_plot = data_plot[:, :, 1:3]
+
+fig, ax1 = plt.subplots()
+ax1.set_xlim(data["sigma"].min(), data["sigma"].max())
+ax1.set_ylim(0, 8)
+line_segments = LineCollection(data_plot, alpha=0.2)
+ax1.add_collection(line_segments)
+
+mean_per_sigma = data.groupby("sigma").mean()
+std_dev_per_sigma = data.groupby("sigma").agg(np.std, ddof=1)
+print(
+    "Maximum is at {} and has dimension {}".format(
+        mean_per_sigma["dim"].idxmax(), mean_per_sigma["dim"].max()
+    )
+)
+ref_sigma = mean_per_sigma["dim"].idxmax()
+
+mean_per_sigma["dim"].plot(ax=ax1, color="tab:orange")
+plt.fill_between(
+    mean_per_sigma.index,
+    mean_per_sigma["dim"] + std_dev_per_sigma["dim"],
+    mean_per_sigma["dim"] - std_dev_per_sigma["dim"],
+    alpha=0.2,
+    color="tab:orange",
+)
+plt.xlabel("$\\sigma$")
+plt.ylabel("$d_{\\rm spec}$")
+# %%
+fig.set_size_inches(5.52, 3.41)
+fig.savefig("../plots/out/transitive_percolations_1.pdf", bbox_inches="tight")
 # %%
